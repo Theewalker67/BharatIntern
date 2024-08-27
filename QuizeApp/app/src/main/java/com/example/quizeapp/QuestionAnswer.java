437c9406188aa8388f9d794c3ad542993483ca9e
Theewalker67 <diwakarguptacdr@gmail.com>
2024-08-27 14:27:26 +0530

    Add files via upload
    This is a Quiz app. which provides Questions and their answer and gives result to user.

---
+++ QuizeApp/app/src/main/java/com/example/quizeapp/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.quizeapp;
+
+public class QuestionAnswer {
+
+        public static String question []={
+                "Who is Prime Minister of India",
+                "Who CEO of Google",
+                "what is 12*9 ?",
+                "who is the founder of Tesla?",
+                "In the given options, which is the Example of System Software?",
+                "What is Capital of Russia"
+        };
+        public static String choices [][]={
+                {"Narendra Modi" , "Rahul" , "Mamta" , "Yogi Adityanath"},
+                {"Sundar Pichai" , "Einstein" , "Edison" , "None of the above"},
+                {"96" , "84" , "102" , "108"},
+                {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+                {"Windows" , "Linux" , "MacOS" , "All of the above"},
+                {"London","Lahore","Moscow","Lucknow"}
+        };
+        public static String correctAnswers []={
+                "Narendra Modi" ,
+                "Sundar Pichai",
+                "108",
+                "Elon Musk",
+                "All of the above",
+                "Moscow"};
+    }
+
